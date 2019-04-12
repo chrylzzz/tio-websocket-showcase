c9860ffe9bcc47a165c7e0db16ae0f8ad2ae58f4
chr0527 <1015670600@qq.com>
2019-04-12 14:51:12 +0800

    Initial commit

---
+++ src/main/java/org/tio/showcase/server/Const.java
@@ -0,0 +1,11 @@
+package org.tio.showcase.server;
+
+/**
+ * @author tanyaowu
+ */
+public class Const {
+    /**
+     * 用于群聊的group id
+     */
+    public static final String GROUP_ID = "showcase-websocket";
+}
